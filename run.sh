1b0ce94c90fdf1672264c25a9bb7842f53cffddb
Wonsup Yoon <pusnow@kaist.ac.kr>
2023-01-15 02:12:14 +0900

    refactor install-bash/zsh

--- run.sh
+++ run.sh
@@ -65,10 +65,10 @@ install-man-dir() {
 }
 
 install-bash-c() {
-    cp -prv "${1}" "${BUILD_PATH}/share/bash_completion.d/"
+    cp -prv "${2}" "${BUILD_PATH}/share/bash_completion.d/${1}.bash"
 }
 install-zsh-c() {
-    cp -prv "${1}" "${BUILD_PATH}/share/zsh_completion.d/"
+    cp -prv "${2}" "${BUILD_PATH}/share/zsh_completion.d/_${1}"
 }
 
 download-install-man() {
@@ -77,13 +77,13 @@ download-install-man() {
 }
 
 download-install-bash-c() {
-    wget -qO "/tmp/${1}" "${2}"
-    install-bash-c "/tmp/${1}"
+    wget -qO "/tmp/${1}.bash" "${2}"
+    install-bash-c "${1}" "/tmp/${1}.bash"
 }
 
 download-install-zsh-c() {
-    wget -qO "/tmp/${1}" "${2}"
-    install-zsh-c "/tmp/${1}"
+    wget -qO "/tmp/_${1}" "${2}"
+    install-zsh-c "${1}" "/tmp/_${1}"
 }
 
 if [[ ! -f versions ]]; then
@@ -166,15 +166,15 @@ install() {
     wget -qO "${BUILD_PATH}/share/zsh_plugin.d/fzf-keybindings.zsh" "https://raw.githubusercontent.com/junegunn/fzf/${FZF_VERSION}/shell/key-bindings.zsh"
     download-install-man 1 fzf.1 "https://raw.githubusercontent.com/junegunn/fzf/${FZF_VERSION}/man/man1/fzf.1"
     download-install-man 1 fzf-tmux.1 "https://raw.githubusercontent.com/junegunn/fzf/${FZF_VERSION}/man/man1/fzf-tmux.1"
-    download-install-bash-c fzf.bash "https://raw.githubusercontent.com/junegunn/fzf/${FZF_VERSION}/shell/completion.bash"
-    download-install-zsh-c _fzf "https://raw.githubusercontent.com/junegunn/fzf/${FZF_VERSION}/shell/completion.zsh"
+    download-install-bash-c fzf "https://raw.githubusercontent.com/junegunn/fzf/${FZF_VERSION}/shell/completion.bash"
+    download-install-zsh-c fzf "https://raw.githubusercontent.com/junegunn/fzf/${FZF_VERSION}/shell/completion.zsh"
 
     install-bin "${EXTRACT_PATH}/hexyl/hexyl"
     install-man 1 "${EXTRACT_PATH}/hexyl/hexyl.1"
 
     install-bin "${EXTRACT_PATH}/delta/delta"
-    download-install-bash-c delta.bash "https://raw.githubusercontent.com/dandavison/delta/${DELTA_VERSION}/etc/completion/completion.bash"
-    download-install-zsh-c _delta "https://raw.githubusercontent.com/dandavison/delta/${DELTA_VERSION}/etc/completion/completion.zsh"
+    download-install-bash-c delta "https://raw.githubusercontent.com/dandavison/delta/${DELTA_VERSION}/etc/completion/completion.bash"
+    download-install-zsh-c delta "https://raw.githubusercontent.com/dandavison/delta/${DELTA_VERSION}/etc/completion/completion.zsh"
 
     if [[ "${ARCH}" == "x64" ]]; then
         install-all "${EXTRACT_PATH}/nvim"
@@ -197,15 +197,15 @@ install() {
 
     install-bin "${EXTRACT_PATH}/fd/fd"
     install-man 1 "${EXTRACT_PATH}/fd/fd.1"
-    install-bash-c "${EXTRACT_PATH}/fd/autocomplete/fd.bash"
-    install-zsh-c "${EXTRACT_PATH}/fd/autocomplete/_fd"
+    install-bash-c fd "${EXTRACT_PATH}/fd/autocomplete/fd.bash"
+    install-zsh-c fd "${EXTRACT_PATH}/fd/autocomplete/_fd"
 
     install-all "${EXTRACT_PATH}/gh"
 
     if [[ "${ARCH}" == "x64" ]]; then
         install-bin "${EXTRACT_PATH}/rg/rg"
-        install-bash-c "${EXTRACT_PATH}/rg/complete/rg.bash"
-        install-zsh-c "${EXTRACT_PATH}/rg/complete/_rg"
+        install-bash-c rg "${EXTRACT_PATH}/rg/complete/rg.bash"
+        install-zsh-c rg "${EXTRACT_PATH}/rg/complete/_rg"
     fi
 
     install-bin "${EXTRACT_PATH}/kubectl/kubectl"
@@ -218,12 +218,9 @@ install() {
     install-man-dir 1 "${EXTRACT_PATH}/cmake/man/man1"
     install-man-dir 7 "${EXTRACT_PATH}/cmake/man/man7"
 
-    install-bash-c "${EXTRACT_PATH}/cmake/share/bash-completion/completions/cmake"
-    mv "${BUILD_PATH}/share/bash_completion.d/cmake" "${BUILD_PATH}/share/bash_completion.d/cmake.bash"
-    install-bash-c "${EXTRACT_PATH}/cmake/share/bash-completion/completions/cpack"
-    mv "${BUILD_PATH}/share/bash_completion.d/cpack" "${BUILD_PATH}/share/bash_completion.d/cpack.bash"
-    install-bash-c "${EXTRACT_PATH}/cmake/share/bash-completion/completions/ctest"
-    mv "${BUILD_PATH}/share/bash_completion.d/ctest" "${BUILD_PATH}/share/bash_completion.d/ctest.bash"
+    install-bash-c cmake "${EXTRACT_PATH}/cmake/share/bash-completion/completions/cmake"
+    install-bash-c cpack "${EXTRACT_PATH}/cmake/share/bash-completion/completions/cpack"
+    install-bash-c ctest "${EXTRACT_PATH}/cmake/share/bash-completion/completions/ctest"
     cp -prv "${EXTRACT_PATH}/cmake/share/cmake-"* "${BUILD_PATH}/share/"
 
     install-bin "${EXTRACT_PATH}/hugo/hugo"
@@ -245,8 +242,8 @@ install() {
     install-bin "${EXTRACT_PATH}/dasel/dasel"
 
     install-bin "${EXTRACT_PATH}/btm/btm"
-    install-bash-c "${EXTRACT_PATH}/btm/completion/btm.bash"
-    install-zsh-c "${EXTRACT_PATH}/btm/completion/_btm"
+    install-bash-c btm "${EXTRACT_PATH}/btm/completion/btm.bash"
+    install-zsh-c btm "${EXTRACT_PATH}/btm/completion/_btm"
 
     if [[ "${ARCH}" == "x64" ]]; then
         install-all "${EXTRACT_PATH}/zstd"
@@ -269,41 +266,41 @@ gen-completions() {
     if [[ -x "${BUILD_PATH}/bin/rclone" ]]; then
         "${BUILD_PATH}/bin/rclone" completion bash >/tmp/rclone.bash
         "${BUILD_PATH}/bin/rclone" completion zsh >/tmp/_rclone
-        install-bash-c "/tmp/rclone.bash"
-        install-zsh-c "/tmp/_rclone"
+        install-bash-c rclone "/tmp/rclone.bash"
+        install-zsh-c rclone "/tmp/_rclone"
     fi
 
     if [[ -x "${BUILD_PATH}/bin/pandoc" ]]; then
         "${BUILD_PATH}/bin/pandoc" --bash-completion >/tmp/pandoc.bash
-        install-bash-c "/tmp/pandoc.bash"
+        install-bash-c pandoc "/tmp/pandoc.bash"
     fi
 
     if [[ -x "${BUILD_PATH}/bin/gh" ]]; then
         ${BUILD_PATH}/bin/gh completion -s bash >"/tmp/gh.bash"
         ${BUILD_PATH}/bin/gh completion -s zsh >"/tmp/_gh"
-        install-bash-c "/tmp/gh.bash"
-        install-zsh-c "/tmp/_gh"
+        install-bash-c gh "/tmp/gh.bash"
+        install-zsh-c gh "/tmp/_gh"
     fi
 
     if [[ -x "${BUILD_PATH}/bin/kubectl" ]]; then
         ${BUILD_PATH}/bin/kubectl completion bash >"/tmp/kubectl.bash"
         ${BUILD_PATH}/bin/kubectl completion zsh >"/tmp/_kubectl"
-        install-bash-c "/tmp/kubectl.bash"
-        install-zsh-c "/tmp/_kubectl"
+        install-bash-c kubectl "/tmp/kubectl.bash"
+        install-zsh-c kubectl "/tmp/_kubectl"
     fi
 
     if [[ -x "${BUILD_PATH}/bin/hugo" ]]; then
         ${BUILD_PATH}/bin/hugo completion bash >"/tmp/hugo.bash"
         ${BUILD_PATH}/bin/hugo completion zsh >"/tmp/_hugo"
-        install-bash-c "/tmp/hugo.bash"
-        install-zsh-c "/tmp/_hugo"
+        install-bash-c hugo "/tmp/hugo.bash"
+        install-zsh-c hugo "/tmp/_hugo"
     fi
 
     if [[ -x "${BUILD_PATH}/bin/dasel" ]]; then
         ${BUILD_PATH}/bin/dasel completion bash >"/tmp/dasel.bash"
         ${BUILD_PATH}/bin/dasel completion zsh >"/tmp/_dasel"
-        install-bash-c "/tmp/dasel.bash"
-        install-zsh-c "/tmp/_dasel"
+        install-bash-c dasel "/tmp/dasel.bash"
+        install-zsh-c dasel "/tmp/_dasel"
     fi
 }
 
